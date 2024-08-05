839cb59859b36599c359e03d2bfcb55be9c418ac
4Shage <47282189+4Shage@users.noreply.github.com>
2024-08-05 19:55:24 +0200

    u0508

--- Manager.gd
+++ Manager.gd
@@ -4,21 +4,27 @@ var date : Dictionary
 
 const SC : Dictionary = {
 	"menu":"res://menu.tscn",
-	"cal":"res://calendar.tscn",
+	#"cal":"res://calendar.tscn",
 	"ushub":"res://users_hub.tscn",
 	"us":"res://user.tscn"
 }
 func chScene(sc:String):
-	get_tree().change_scene_to_file(SC.get(sc))
+	if SC.has(sc):
+		get_tree().change_scene_to_file(SC.get(sc))
+	else:
+		printerr("Scene doesn't exist : "+sc)
 
 const monthsDays : Array = [31,28,31,30,31,30,31,31,30,31,30,31]
 
 var selUser : String
 var users : Dictionary = {}
+var db : Dictionary = {}
 var monthDays : int
 
-func addUser(namE:String,shortName:String,cost:float,startDate:String,endDate:String,dayOn:Dictionary,wild:Dictionary={},dayOff:Dictionary={}):
-	users[shortName] = {
+func addUser(namE:String,shortName:String,cost:float = 0,startDate:String = "",endDate:String = "",dayOn:Dictionary={},wild:Dictionary={},dayOff:Dictionary={}):
+	var rng := RandomNumberGenerator.new()
+	users[rng.seed] = {
+		"shortName":shortName,
 		"name":namE,
 		"cost":cost,
 		"borrowed":"",
@@ -34,7 +40,13 @@ func remUser(shortName:String):
 func calcMonthlyCost(shortName:String):
 	pass
 
+func userAddToDB():
+	db = {}
+	for e in users:
+		db[users[e["shortName"]]] = e
+
 func _ready():
+	userAddToDB()
 	date = Time.get_date_dict_from_system()
 	print_debug(date)
 	monthDays = monthsDays[date["month"]-1]
